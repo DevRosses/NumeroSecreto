010c6f94b7600597628e837ec718b73c62be2f0b
devrosses <devrosses@gmail.com>
2025-01-26 22:47:22 -0300

    agregar función para establecer condiciones iniciales y reiniciar juego

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
 let numeroMaximo = 10;
-let numeroSecreto = generarNumeroSecreto();
-let intentos = 1;
+let numeroSecreto = 0;
+let intentos = 0;
 let maximosIntentos = 3;
 
 function verificarIntento() {
+  //Tomo valor del input.
   let numeroUsuario = parseInt(document.getElementById("valorUsuario").value);
+  //veo el valor de input en consola.
   console.log(numeroUsuario);
   console.log(numeroSecreto);
 
-  if (numeroUsuario == numeroSecreto) {
+  if (numeroUsuario === numeroSecreto) {
     asignarTextoElemento(
       "p",
       `Acertaste! Lo lograste en ${intentos} ${
         intentos == 1 ? "intento" : "intentos"
       }.`
     );
+    //Quita atributo(disabled) al button nuevo juego.
+    document.getElementById("reiniciar").removeAttribute("disabled");
   } else {
     //El usuario no acertó.
     if (numeroUsuario > numeroSecreto) {
@@ -32,22 +36,33 @@ function verificarIntento() {
     }
   }
   return;
-};
+}
 
 //Limpio Input
 function limpiarCaja() {
-  let valorCaja = document.querySelector('#valorUsuario');
-  valorCaja.value = '';
+  document.querySelector("#valorUsuario").value = "";
 }
 
 function generarNumeroSecreto() {
   return Math.floor(Math.random() * numeroMaximo) + 1;
-};
+}
 
 function asignarTextoElemento(elemento, texto) {
   let titulo = document.querySelector(elemento);
   titulo.innerHTML = texto;
-};
+}
 
-asignarTextoElemento("h1", "Juego del número secreto");
-asignarTextoElemento("p", `Indicame un numero entre 1 y ${numeroMaximo}`);
+function condicionesIniciales() {
+  asignarTextoElemento("h1", "Juego del número secreto");
+  asignarTextoElemento("p", `Indicame un numero entre 1 y ${numeroMaximo}`);
+  numeroSecreto = generarNumeroSecreto();
+  intentos = 1;
+}
+
+function reiniciarJuego() {
+  limpiarCaja();
+  condicionesIniciales();
+  document.getElementById("reiniciar").setAttribute("disabled",true);
+}
+
+condicionesIniciales();
