63b45df868195a01505eec9369841babae1624f6
devrosses <devrosses@gmail.com>
2025-01-26 22:10:04 -0300

    agregar función para limpiar input

--- app.js
+++ app.js
@@ -7,46 +7,47 @@ function verificarIntento() {
   let numeroUsuario = parseInt(document.getElementById("valorUsuario").value);
   console.log(numeroUsuario);
   console.log(numeroSecreto);
-  console.log(numeroSecreto == numeroUsuario);
-  
 
-  while (numeroUsuario != numeroSecreto) {
-
-    if (numeroUsuario == numeroSecreto) {
+  if (numeroUsuario == numeroSecreto) {
+    asignarTextoElemento(
+      "p",
+      `Acertaste! Lo lograste en ${intentos} ${
+        intentos == 1 ? "intento" : "intentos"
+      }.`
+    );
+  } else {
+    //El usuario no acertó.
+    if (numeroUsuario > numeroSecreto) {
+      asignarTextoElemento("p", "El número secreto es menor");
+    } else {
+      asignarTextoElemento("p", "El número secreto es mayor");
+    }
+    intentos++;
+    limpiarCaja();
+    if (intentos > maximosIntentos) {
       asignarTextoElemento(
         "p",
-        `Acertaste, el número es ${numeroUsuario}. Lo lograste en ${intentos} ${
-          intentos == 1 ? "vez" : "veces"
-        }.`
+        `llegaste al número máximo de ${maximosIntentos} intentos`
       );
-    } else {
-      if (numeroUsuario > numeroSecreto) {
-        asignarTextoElemento("p", "El número secreto es menor");
-      } else {
-        asignarTextoElemento("p", "El número secreto es mayor");
-      }
-      intentos++;
-      if (intentos > maximosIntentos) {
-        asignarTextoElemento(
-          "p",
-          `llegaste al número máximo de ${maximosIntentos} intentos`
-        );
-
-        break;
-      }
     }
   }
   return;
-}
+};
 
-function asignarTextoElemento(elemento, texto) {
-  let titulo = document.querySelector(elemento);
-  titulo.innerHTML = texto;
+//Limpio Input
+function limpiarCaja() {
+  let valorCaja = document.querySelector('#valorUsuario');
+  valorCaja.value = '';
 }
 
 function generarNumeroSecreto() {
   return Math.floor(Math.random() * numeroMaximo) + 1;
-}
+};
+
+function asignarTextoElemento(elemento, texto) {
+  let titulo = document.querySelector(elemento);
+  titulo.innerHTML = texto;
+};
 
 asignarTextoElemento("h1", "Juego del número secreto");
 asignarTextoElemento("p", `Indicame un numero entre 1 y ${numeroMaximo}`);
